663af9b9c0bb03b4d6fde68ccd855bcea31e7653
John  Tucker <john@larkintuckerllc.com>
2017-12-19 08:41:53 -0500

    sleep worker

--- hello/worker.js
+++ hello/worker.js
@@ -1,9 +1,11 @@
 /* eslint-disable no-console */
 const kue = require('kue');
+const { sleep } = require('sleep');
 
 const queue = kue.createQueue();
 console.log('WORKER CONNECTED');
 queue.process('mytype', (job, done) => {
+  sleep(5);
   console.log('WORKER JOB COMPLETE');
   switch (job.data.letter) {
     case 'a':
